9da5913517675afe280660c4c02ace592b8c2e6b
uf-code <ugur.firat.developer@gmail.com>
2022-07-07 23:25:18 +0300

    creating home screen

---
+++ frontent-mobile/screens/HomeScreen.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const HomeScreen = () => {
+  return <Text className='text-red-500'>HomeScreen</Text>
+}
+
+export default HomeScreen
